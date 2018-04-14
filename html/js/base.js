c08e2cf1a12b6ca1d5fc6b255363836d40e5727e
gamedrm <36598123+gamedrm@users.noreply.github.com>
2018-04-14 08:07:10 +0800

    header

--- html/js/base.js
+++ html/js/base.js
@@ -1,3 +1,11 @@
-let oFuncs = {
 
-};
+$(function() {
+  // 搜索框
+  $('span.nav-search-icon').find('img').click(function() {
+    $(this).prev('input').fadeToggle(300);
+  });
+  // 移动端导航栏显示隐藏
+  $('span.nav-menu-icon').click(function() {
+    $('ul.main-nav-list').fadeToggle(300);
+  })
+})
